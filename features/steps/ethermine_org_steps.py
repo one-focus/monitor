255a04d69dd1ccd93075375370f05032facb79af
sozdai <sash.kardash@gmail.com>
2021-11-13 22:43:51 +0400

    add tests

---
+++ features/steps/ethermine_org_steps.py
@@ -0,0 +1,11 @@
+from behave import *
+import requests
+
+
+@when('monitor "{address}" hashrate')
+def monitor_hashrate(context, address):
+    response = requests.get(f'https://api.ethermine.org/miner/{address}/dashboard').json()
+    if response['status'] != 'OK' or float(response['data']['statistics'][0]['reportedHashrate']) * 0.000001 < 220:
+        context.driver.get(f'https://ethermine.org/miners/{address}/dashboard')
+        context.page_name = f'https://ethermine.org/miners/{address}/dashboard'
+        raise RuntimeError(f"Hash rate is: {float(response['data']['statistics'][0]['reportedHashrate']) * 0.000001}")
---
+++ features/tests/ethermine.feature
@@ -0,0 +1,7 @@
+# Created by kardash at 13.11.21
+@regression
+Feature: Miner
+  # Enter feature description here
+
+  Scenario: Monitor ethermine
+    When monitor "60575Ec21cEa8432cbd722D64e43026B1C1f72dE" hashrate
