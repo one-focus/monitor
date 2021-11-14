16e8c1c721b5f146eaba21f634d38e7b027c8a58
sozdai <sash.kardash@gmail.com>
2021-11-14 22:48:10 +0400

    add sleep

--- features/steps/ethermine_org_steps.py
+++ features/steps/ethermine_org_steps.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from behave import *
 import requests
 
@@ -7,5 +9,6 @@ def monitor_hashrate(context, address):
     response = requests.get(f'https://api.ethermine.org/miner/{address}/dashboard').json()
     if response['status'] != 'OK' or float(response['data']['statistics'][0]['reportedHashrate']) * 0.000001 < 220:
         context.driver.get(f'https://ethermine.org/miners/{address}/dashboard')
+        sleep(2)
         context.page_name = f'https://ethermine.org/miners/{address}/dashboard'
         raise RuntimeError(f"Hash rate is: {float(response['data']['statistics'][0]['reportedHashrate']) * 0.000001}")
