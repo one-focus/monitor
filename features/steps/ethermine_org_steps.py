d9cd7607d7c48893afce9bbae8b58544786b9e6d
sozdai <sash.kardash@gmail.com>
2022-01-25 11:06:32 +0300

    update hiveon method

--- features/steps/ethermine_org_steps.py
+++ features/steps/ethermine_org_steps.py
@@ -6,7 +6,8 @@ import requests
 
 @when('monitor "{address}" hashrate')
 def monitor_hashrate(context, address):
-    response = requests.get(f'https://hiveon.net/api/v1/stats/miner/{address.lower}/ETH').json()
-    if 'sharesStatusStats' in response or float(response['reportedHashrate']) * 0.000001 < 300:
+    response = requests.get(f'https://hiveon.net/api/v1/stats/miner/{address.lower()}/ETH').json()
+    if 'reportedHashrate' not in response or float(response['reportedHashrate']) * 0.000001 < 300:
         context.driver.get(f'https://hiveon.net/eth_ru?miner=0x{address}')
+        sleep(5)
         raise RuntimeError(f"Hash rate is: low")
