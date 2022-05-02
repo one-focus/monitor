bcda7cdbb3580c9323c63e1457d7c929147a330c
Alex Kardash <47696119+one-focus@users.noreply.github.com>
2022-05-02 10:47:37 +0300

    Update ethermine_org_steps.py

--- features/steps/ethermine_org_steps.py
+++ features/steps/ethermine_org_steps.py
@@ -7,7 +7,7 @@ import requests
 @when('monitor "{address}" hashrate')
 def monitor_hashrate(context, address):
     response = requests.get(f'https://hiveon.net/api/v1/stats/miner/{address.lower()}/ETH').json()
-    if 'reportedHashrate' not in response or float(response['reportedHashrate']) * 0.000001 < 280:
+    if 'reportedHashrate' not in response or float(response['reportedHashrate']) * 0.000001 < 270:
         context.driver.get(f'https://hiveon.net/eth_ru?miner=0x{address}')
         sleep(5)
         raise RuntimeError(f"Hash rate is: low")
