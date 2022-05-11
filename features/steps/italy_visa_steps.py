760ae463e07364062dae9465e6a02ca50bb1a7d2
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-05-11 15:49:41 +0300

    update visa

--- features/steps/italy_visa_steps.py
+++ features/steps/italy_visa_steps.py
@@ -56,7 +56,7 @@ def login_visa(context):
             break
     else:
         raise RuntimeError(f'Unable to login. Error is {context.current_page.get_text(messages)}')
-    context.current_page.hover_element((By.XPATH, '//div[@id="timeTable"]/following-sibling::p/a'))
+    context.current_page.hover_element((By.XPATH, '//div[@id="timeTable"]//a'))
 
 
 def login(context, email, password):
