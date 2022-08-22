f147032435e0410ac62b02f7265090ca55ec5f39
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-08-22 07:47:19 +0300

    update hiveon scan

--- features/steps/ethermine_org_steps.py
+++ features/steps/ethermine_org_steps.py
@@ -6,8 +6,8 @@ import requests
 
 @when('monitor "{address}" hashrate')
 def monitor_hashrate(context, address):
-    response = requests.get(f'https://hiveon.net/api/v1/stats/miner/{address.lower()}/ETH').json()
-    if 'reportedHashrate' not in response or float(response['reportedHashrate']) * 0.000001 < 270:
-        context.driver.get(f'https://hiveon.net/eth_ru?miner=0x{address}')
+    context.driver.get(f'https://hiveon.net/eth_ru?miner=0x{address}')
+    text = context.driver.find_element_by_xpath('//div[text()="В реальном времени"]/..//span[1]').text
+    if float(text) < 270:
         sleep(5)
-        raise RuntimeError(f"Hash rate is: low")
+        raise RuntimeError(f"Hash rate is: {text}, expected 270+")
