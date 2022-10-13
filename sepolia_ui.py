9a30e1acb5004dcb12aa7fce5935ef31180e78d2
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-10-13 14:29:36 +0300

    add sepolia.py

--- sepolia_ui.py
+++ sepolia_ui.py
@@ -6,13 +6,17 @@ import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.headless = True
-driver = uc.Chrome(options=options, use_subprocess=True)
-driver.get('https://faucet-sepolia.rockx.com/')
-driver.find_element(By.XPATH, '//input[@placeholder="Paste the tweet URL here"]').send_keys(sys.argv[1])
-driver.find_element(By.XPATH, '//button[text()="Send Me ETH"]').click()
-sleep(3)
-text = driver.find_element(By.XPATH, '//div[contains(@class, "arco-message-wrapper-top")]').text
-print(text)
+
+
+if __name__ == '__main__':
+    # upload_basic()
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = uc.Chrome(options=options)
+    driver.get('https://faucet-sepolia.rockx.com/')
+    driver.find_element(By.XPATH, '//input[@placeholder="Paste the tweet URL here"]').send_keys(sys.argv[1])
+    driver.find_element(By.XPATH, '//button[text()="Send Me ETH"]').click()
+    sleep(3)
+    text = driver.find_element(By.XPATH, '//div[contains(@class, "arco-message-wrapper-top")]').text
+    print(text)
 
