5413598dc51f75b79974e2a4104c15c130848552
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-10-13 14:55:36 +0300

    add sepolia.py

--- sepolia_ui.py
+++ sepolia_ui.py
@@ -1,25 +1,21 @@
 from time import sleep
-
 import sys
 
 import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 if __name__ == '__main__':
-    # upload_basic()
     options = webdriver.ChromeOptions()
     options.headless = True
     driver = uc.Chrome(options=options)
     driver.get('https://faucet-sepolia.rockx.com/')
     driver.find_element(By.XPATH, '//input[@placeholder="Paste the tweet URL here"]').send_keys(sys.argv[1])
     driver.find_element(By.XPATH, '//button[text()="Send Me ETH"]').click()
-    sleep(3)
-    print('===')
-    print(driver.page_source)
-    print('===')
-    text = driver.find_element(By.XPATH, '//div[contains(@class, "arco-message-wrapper-top")]').text
-    print(text)
-
+    locator = By.XPATH, '//div[contains(@class, "arco-message-wrapper-top")]'
+    expected_condition = ec.presence_of_element_located(locator)
+    result = WebDriverWait(driver, 10).until(expected_condition, message=f'Не могу найти')
+    print(result.text)
+    #'Funding request accepted'
