0b4bcbe06640f5a6d94539105e766e98cf09253f
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-10-13 14:31:24 +0300

    add sepolia.py

--- sepolia_ui.py
+++ sepolia_ui.py
@@ -17,6 +17,9 @@ if __name__ == '__main__':
     driver.find_element(By.XPATH, '//input[@placeholder="Paste the tweet URL here"]').send_keys(sys.argv[1])
     driver.find_element(By.XPATH, '//button[text()="Send Me ETH"]').click()
     sleep(3)
+    print('===')
+    print(driver.page_source)
+    print('===')
     text = driver.find_element(By.XPATH, '//div[contains(@class, "arco-message-wrapper-top")]').text
     print(text)
 
